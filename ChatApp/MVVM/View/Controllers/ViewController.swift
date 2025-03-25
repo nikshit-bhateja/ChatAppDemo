d330ab4e05cf3257b546ac1242a120b291d9bc6a
Nikshit Bhateja <iosdevelopernikshit@gmail.com>
2025-03-25 22:23:42 +0530

    Initial Commit

---
+++ ChatApp/MVVM/View/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Nikk Bhateja on 25/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
